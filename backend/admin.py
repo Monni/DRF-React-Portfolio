3c28269d0f9c4695cecabebe336bf084bf0ffc3e
Miika <miika_avela@hotmail.com>
2018-06-14 16:17:36 +0300

    Register Career to admin site

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from backend.models import Project, Event, Education, Document, Image, Tag
+from backend.models import Project, Event, Education, Document, Image, Tag, Career
 
 admin.site.register(Project)
 admin.site.register(Event)
@@ -9,3 +9,4 @@ admin.site.register(Education)
 admin.site.register(Document)
 admin.site.register(Image)
 admin.site.register(Tag)
+admin.site.register(Career)
