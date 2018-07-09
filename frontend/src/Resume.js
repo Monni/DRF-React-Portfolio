03da956074563a40494cbcbedc9f1b0f4a04ff27
Miika <miika_avela@hotmail.com>
2018-07-09 19:54:17 +0300

    Dynamic work experience loading, clean old static content

--- frontend/src/Resume.js
+++ frontend/src/Resume.js
@@ -23,89 +23,11 @@ export default class Resume extends React.Component {
                     </div>
                 </section>
 
-                <div>
-                    <ResumeLoader type='education' subType='EDU'/>
-                </div>
-
                 {/* Education */}
-                <section className={ MainStyles.module }>
-                    <div className={ [MainStyles.container_fluid, MainStyles.container_custom].join(' ') }>
-                        <div className={ MainStyles.row }>
-                            <div className={ MainStyles.col_sm_4 }>
-                                <h6>Education.</h6>
-                            </div>
-
-                            {/* TODO this needs to be a dynamic component fetching info from DB */}
-
-
-
-                            <div className={ MainStyles.col_sm_8 }>
-                                <ul className={ styles.resumeInfo }>
-                                    <li>
-                                        <h6 className={ styles.resumeTitle }>Academy of Art University</h6>
-                                        <h6 className={ styles.resumeMeta }>2001 - 2004</h6>
-                                        <p>Academy of Art University's School of Web Design & New Media is the
-                                            intersection between traditional design and new technologies.</p>
-                                    </li>
-                                    <li>
-                                        <h6 className={ styles.resumeTitle }>Academy of Art University</h6>
-                                        <h6 className={ styles.resumeMeta }>2001 - 2004</h6>
-                                        <p>Information technology (IT) workers can be found in many types of
-                                            organizations. According to the U.S. Department of Labor, "In the modern
-                                            workplace, it is imperative that Information Technology (IT) works both
-                                            effectively and reliably.</p>
-                                    </li>
-                                    <li>
-                                        <h6 className={ styles.resumeTitle }>Academy of Art University</h6>
-                                        <h6 className={ styles.resumeMeta }>2001 - 2004</h6>
-                                        <p>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit
-                                            auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit
-                                            consequat.</p>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <ResumeLoader type='education' subType='EDU'/>
 
                 {/* Work Experience */}
-                <section className={ MainStyles.module }>
-                    <div className={ [MainStyles.container_fluid, MainStyles.container_custom].join(' ') }>
-                        <div className={ MainStyles.row }>
-                            <div className={ MainStyles.col_sm_4 }>
-                                <h6>Work Experience.</h6>
-                            </div>
-
-                            {/* TODO this needs to be a dynamic component fetching info from DB */}
-
-                            <div className={ MainStyles.col_sm_8 }>
-                                <ul className={ styles.resumeInfo }>
-                                    <li>
-                                        <h6 className={ styles.resumeTitle }>Software Engineer - Apple</h6>
-                                        <h6 className={ styles.resumeMeta }>2001 - 2004</h6>
-                                        <p>Academy of Art University's School of Web Design & New Media is the
-                                            intersection between traditional design and new technologies.</p>
-                                    </li>
-                                    <li>
-                                        <h6 className={ styles.resumeTitle }>Creative Direction - Twitter</h6>
-                                        <h6 className={ styles.resumeMeta }>2005 - 2008</h6>
-                                        <p>Information technology (IT) workers can be found in many types of
-                                            organizations. According to the U.S. Department of Labor, "In the modern
-                                            workplace, it is imperative that Information Technology (IT) works both
-                                            effectively and reliably.</p>
-                                    </li>
-                                    <li>
-                                        <h6 className={ styles.resumeTitle }>Front-end Developer – </h6>
-                                        <h6 className={ styles.resumeMeta }>2009 - 2012</h6>
-                                        <p>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit
-                                            auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit
-                                            consequat.</p>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <ResumeLoader type='career' subType='WORK'/>
 
                 {/* TODO here fetch skills as buzzwords from LinkedIn. (Or DB? Do I need a table for that?) */}
                 <section className={ MainStyles.module }>
